6aa38c36311bc94429f19af3a6a4a5510567ead6
Robert Taylor <rjt903@gmail.com>
2023-03-19 02:02:04 +0000

    Added placeholder feed view

---
+++ vibely/View/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeeView.swift
+//  vibely
+//
+//  Created by Robert on 19/03/2023.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeeView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
