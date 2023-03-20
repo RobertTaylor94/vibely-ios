ca7b3fbb3cb8f30f9a08906a30c849aa3347c6b8
Robert Taylor <rjt903@gmail.com>
2023-03-20 11:54:40 +0000

    Display user data and posts on profileView

--- vibely/View/ProfileView.swift
+++ vibely/View/ProfileView.swift
@@ -10,14 +10,48 @@ import SwiftUI
 struct ProfileView: View {
     
     @EnvironmentObject private var authModel: AuthViewModel
+    @EnvironmentObject private var firestore: FirestoreManager
     
     var body: some View {
-        VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            Button(action: {authModel.signOut()}, label: {
-                Text("Sign Out")
-                    .bold()
-            })
+        NavigationView {
+            VStack {
+                HStack {
+                    Text("Bio")
+                        .font(.title3)
+                        .padding(5)
+                    Text(firestore.userData.bio)
+                }.padding(10)
+                ScrollView {
+                    ForEach(firestore.userPosts) { item in
+                        Post(user: item.displayName, post: item.post)
+                    }
+                }
+                Spacer()
+                Button(action: {
+                    authModel.signOut()
+                }, label: {
+                    Text("Sign Out")
+                        .bold()
+                })
+                
+            }.onAppear {
+                firestore.getCurrentUserData(uid: authModel.user!.uid)
+                firestore.getUserPosts(uid: authModel.user!.uid)
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading, content: {
+                    Button(action: {
+                        authModel.signOut()
+                    }, label: {
+                        Text("Sign Out")
+                            .bold()
+                    })
+                })
+                    ToolbarItem(placement: .principal, content: {
+                        Text(authModel.user!.displayName!)
+                            .font(.headline)
+                    })
+            }
         }
     }
 }
