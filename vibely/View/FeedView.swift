2faaa7f17502942d38fa1a95357567b0c7cba4db
Robert Taylor <rjt903@gmail.com>
2023-03-28 13:01:34 +0100

    Added 'Add Post' button to FeedView

--- vibely/View/FeedView.swift
+++ vibely/View/FeedView.swift
@@ -1,5 +1,5 @@
 //
-//  FeeView.swift
+//  FeedView.swift
 //  vibely
 //
 //  Created by Robert on 19/03/2023.
@@ -12,19 +12,44 @@ struct FeedView: View {
     @State private var selection = "Show All"
     
     var body: some View {
-        VStack {
-            Picker("", selection: $selection) {
-                Text("Show All").tag("Show All")
-                Text("Show Followers").tag("Show Followers")
+        ZStack {
+            VStack {
+                Picker("", selection: $selection) {
+                    Text("Show All").tag("Show All")
+                    Text("Show Followers").tag("Show Followers")
+                }
+                .pickerStyle(.segmented)
+                .padding(50)
+                Spacer()
             }
-            .pickerStyle(.segmented)
-            Text("Hello, World!")
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        // Add post
+                    }) {
+                        Image(systemName: "plus.circle")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .clipShape(Circle())
+                            .shadow(radius: 5)
+                    }
+                    .padding(.trailing, 20)
+                    .padding(.bottom, 20)
+                }
+            }
         }
+        
         
     }
 }
 
-struct FeeView_Previews: PreviewProvider {
+struct FeedView_Previews: PreviewProvider {
     static var previews: some View {
         FeedView()
     }
